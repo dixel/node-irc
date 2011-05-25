fc3f1c8abb4634c3be04bf4cbe65bd9d67de8497
VAvdiushkin <VAvdiushkin@da424b51-ff14-6541-ab8a-bc061a785226>
2011-05-25 06:36:35 +0000

    converted tabs to spaces, now more readable via web
    git-svn-id: http://domprog.com:9000/svn/exmashina/IRC/trunk@22 da424b51-ff14-6541-ab8a-bc061a785226

--- binding.cc
+++ binding.cc
@@ -10,8 +10,8 @@ using namespace v8;
 
 typedef struct message
 {
-	const char *origin;
-	const char **params;
+    const char *origin;
+    const char **params;
 } mess;
 
 void rec_cb(irc_session_t *session, const char *event, const char *origin, const char **params, unsigned int count);
@@ -33,25 +33,25 @@ struct ev_async eio_rc;
 //ConCB - a callback for "connected" event on IRC server
 static Handle<Value> CreateSession(const Arguments &args)
 {
-	printf("Create session\n");
-	_callbacks.event_channel = rec_cb;
-	_callbacks.event_connect = con_cb;
-	_session = irc_create_session(&_callbacks);
-	if (args[0]->IsFunction()) 
-	{ 
-		Local<Function> concb = Function::Cast(*args[0]);
-		if (concb->IsFunction())
-		{
-			ConCB = Persistent<Function>::New(concb);
-		}
-	}
-	if (args[1]->IsFunction()) 
-	{ 
-		Local<Function> reccb = Function::Cast(*args[1]);
-		if (reccb->IsFunction())
-		{
-			RecCB = Persistent<Function>::New(reccb);
-		} }
+    printf("Create session\n");
+    _callbacks.event_channel = rec_cb;
+    _callbacks.event_connect = con_cb;
+    _session = irc_create_session(&_callbacks);
+    if (args[0]->IsFunction()) 
+    { 
+        Local<Function> concb = Function::Cast(*args[0]);
+        if (concb->IsFunction())
+        {
+            ConCB = Persistent<Function>::New(concb);
+        }
+    }
+    if (args[1]->IsFunction()) 
+    { 
+        Local<Function> reccb = Function::Cast(*args[1]);
+        if (reccb->IsFunction())
+        {
+            RecCB = Persistent<Function>::New(reccb);
+        } }
 }
 
 //JS equivalent for irc_connect command
@@ -59,14 +59,14 @@ static Handle<Value> CreateSession(const Arguments &args)
 //Connect(String server, Number port, String password, String nick, String username, String realname)
 static Handle<Value> Connect(const Arguments &args)
 {
-	printf("Connecting...\n");
-	V8STR server(args[0]);
-	int port = args[1]->Int32Value();
-	V8STR password(args[2]);
-	V8STR nick(args[3]);
-	V8STR username(args[4]);
-	V8STR realname(args[5]);
-	printf("connect: %d\n", irc_connect(_session, *server, port, *password, *nick, *username, *realname));
+    printf("Connecting...\n");
+    V8STR server(args[0]);
+    int port = args[1]->Int32Value();
+    V8STR password(args[2]);
+    V8STR nick(args[3]);
+    V8STR username(args[4]);
+    V8STR realname(args[5]);
+    printf("connect: %d\n", irc_connect(_session, *server, port, *password, *nick, *username, *realname));
 }
 
 //JS equivalent for irc_run command
@@ -74,16 +74,16 @@ static Handle<Value> Connect(const Arguments &args)
 //No
 static Handle<Value> Run(const Arguments &args)
 {
-	pthread_t thread;
-	//pthread_t looper;
-	ev_async_init(&eio_nt, con_cb_ev);
-	ev_async_start(EV_DEFAULT_UC_ &eio_nt);
-	ev_async_init(&eio_rc, rec_cb_ev);
-	ev_async_start(EV_DEFAULT_UC_ &eio_rc);
-	pthread_create(&thread, NULL, run_thr, NULL);
-	ev_loop(EV_DEFAULT_ 0);
-	printf("works after loop!\n");
-	pthread_join(thread, NULL);
+    pthread_t thread;
+    //pthread_t looper;
+    ev_async_init(&eio_nt, con_cb_ev);
+    ev_async_start(EV_DEFAULT_UC_ &eio_nt);
+    ev_async_init(&eio_rc, rec_cb_ev);
+    ev_async_start(EV_DEFAULT_UC_ &eio_rc);
+    pthread_create(&thread, NULL, run_thr, NULL);
+    ev_loop(EV_DEFAULT_ 0);
+    printf("works after loop!\n");
+    pthread_join(thread, NULL);
 }
 
 //JS equivalent for irc_cmd_join command
@@ -91,10 +91,10 @@ static Handle<Value> Run(const Arguments &args)
 //Join(String channel, String key)
 static Handle<Value> Join(const Arguments &args)
 {
-	printf("Joininng...\n");
-	V8STR chan(args[0]);
-	V8STR key(args[1]);
-	printf("join: %d\n", irc_cmd_join(_session, *chan, *key));
+    printf("Joininng...\n");
+    V8STR chan(args[0]);
+    V8STR key(args[1]);
+    printf("join: %d\n", irc_cmd_join(_session, *chan, *key));
 }
 
 //JS equivalent for irc_cmd_msg command
@@ -102,68 +102,68 @@ static Handle<Value> Join(const Arguments &args)
 //SendMsg(String dest, String text)
 static Handle<Value> SendMsg(const Arguments &args)
 {
-	printf("Sending message...\n");
-	V8STR chan(args[0]);
-	V8STR message(args[1]);
-	irc_cmd_msg(_session, *chan, *message);
+    printf("Sending message...\n");
+    V8STR chan(args[0]);
+    V8STR message(args[1]);
+    irc_cmd_msg(_session, *chan, *message);
 }
 
 void *run_thr(void *vptr_args)
 {
-	HandleScope scope;
-	printf("Running irc session\n");
-	printf("run: %d", irc_run(_session));
+    HandleScope scope;
+    printf("Running irc session\n");
+    printf("run: %d", irc_run(_session));
 }
 
 void rec_cb(irc_session_t *session, const char *event, const char *origin, const char **params, unsigned int count)
 {
-	mess *newm;
-	newm->origin = origin;
-	newm->params = params;
-	ev_set_userdata(newm);
-	ev_async_send(EV_DEFAULT_UC_ &eio_rc);
+    mess *newm;
+    newm->origin = origin;
+    newm->params = params;
+    ev_set_userdata(newm);
+    ev_async_send(EV_DEFAULT_UC_ &eio_rc);
 }
 
 void con_cb(irc_session_t *session, const char *event, const char *origin, const char **params, unsigned int count)
 {
-	printf("connected\n");
-	ev_async_send(EV_DEFAULT_UC_ &eio_nt);
+    printf("connected\n");
+    ev_async_send(EV_DEFAULT_UC_ &eio_nt);
 }
 
 void rec_cb_ev(EV_P_ ev_async *watcher, int revents)
 {
-	mess *parser;
-	parser = (mess *)ev_userdata();
-	Handle<Value> args[2];
-	args[0] = String::New(parser->origin);
-	args[1] = String::New(parser->params[1]);
-	if (RecCB->IsFunction())
-	{
-		Handle<Object> tmp = Object::New();
-		RecCB->Call(tmp, 2, args);
-	}
+    mess *parser;
+    parser = (mess *)ev_userdata();
+    Handle<Value> args[2];
+    args[0] = String::New(parser->origin);
+    args[1] = String::New(parser->params[1]);
+    if (RecCB->IsFunction())
+    {
+        Handle<Object> tmp = Object::New();
+        RecCB->Call(tmp, 2, args);
+    }
 }
 
 void con_cb_ev(EV_P_ ev_async *watcher, int revents)
 {
-	if (ConCB->IsFunction())
-	{
-		Handle<Object> tmp = Object::New();
-		ConCB->Call(tmp, 0, NULL);
-	}
+    if (ConCB->IsFunction())
+    {
+        Handle<Object> tmp = Object::New();
+        ConCB->Call(tmp, 0, NULL);
+    }
 }
 
 
 extern "C" void
 init (Handle<Object> target)
 {
-	HandleScope scope;
-	Local<FunctionTemplate> t = FunctionTemplate::New();
-	Local<Template> proto_t = t->PrototypeTemplate();
-	proto_t->Set("Connect", FunctionTemplate::New(Connect));
-	proto_t->Set("Run", FunctionTemplate::New(Run));
-	proto_t->Set("CreateSession", FunctionTemplate::New(CreateSession));
-	proto_t->Set("Join", FunctionTemplate::New(Join));
-	proto_t->Set("SendMsg", FunctionTemplate::New(SendMsg));
-	target->Set(String::NewSymbol("func"), t->GetFunction());
+    HandleScope scope;
+    Local<FunctionTemplate> t = FunctionTemplate::New();
+    Local<Template> proto_t = t->PrototypeTemplate();
+    proto_t->Set("Connect", FunctionTemplate::New(Connect));
+    proto_t->Set("Run", FunctionTemplate::New(Run));
+    proto_t->Set("CreateSession", FunctionTemplate::New(CreateSession));
+    proto_t->Set("Join", FunctionTemplate::New(Join));
+    proto_t->Set("SendMsg", FunctionTemplate::New(SendMsg));
+    target->Set(String::NewSymbol("func"), t->GetFunction());
 }
--- test.js
+++ test.js
@@ -7,63 +7,63 @@ var nodeirc = require('./build/default/binding');
 irc = nodeirc.func.prototype;
 var connected = 0;
 fs.readFile('./index.html','utf-8',function (err, data) {
-			if (err) throw err;
-			ind = data; });
+            if (err) throw err;
+            ind = data; });
 console.log('starting nodejs');
 
 function ConnectCallback()
 {
-	console.log('Connected');
-	irc.SendMsg("dixel", "Hello dixel, I successfully connected!");
-	irc.Join("#newchan", "");
-	irc.SendMsg("#newchan", "Hello everybody, I successfully joined!");
-	irc.Join("#pieceofp", "");
-	irc.SendMsg("#pieceofp", "Hello, pieces!");
-	return("Hello");
+    console.log('Connected');
+    irc.SendMsg("dixel", "Hello dixel, I successfully connected!");
+    irc.Join("#newchan", "");
+    irc.SendMsg("#newchan", "Hello everybody, I successfully joined!");
+    irc.Join("#pieceofp", "");
+    irc.SendMsg("#pieceofp", "Hello, pieces!");
+    return("Hello");
 }
 
 function RecieveCallback(origin, message)
 {
-	console.log('%s: %s', origin, message);
+    console.log('%s: %s', origin, message);
 }
 
 
 http.createServer(function(request, response){
-	if(request.method == 'GET')
-	{
-		query = require('url').parse(request.url, true);
-		console.log(query.msg);
-		if (query.query.submit == 'Connect')
-		{
-			irc.CreateSession(ConnectCallback, RecieveCallback);
-			irc.Connect("irc.freenode.net", 6667, null, "vasitronen", null, null);
-			connected = 1;
-			response.writeHead(200, {"Content-Type": "text/html"});
-			response.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><body>', encoding = 'utf-8');
-			response.write("<p>Connecting...</p>", encoding='utf-8');
-			response.end(ind);
-		}
-		else if(query.query.submit == 'Send')
-		{
-			console.log('attempting to send');
-			irc.SendMsg("#newchan", query.query.msg);
-			response.writeHead(200, {"Content-Type": "text/html"});
-			response.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><body>', encoding = 'utf-8');
-			response.end(ind);
-		}
-		else
-		{
-			response.writeHead(200, {"Content-Type": "text/html"});
-			response.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><body>', encoding = 'utf-8');
-			response.end(ind);
-		}
+    if(request.method == 'GET')
+    {
+        query = require('url').parse(request.url, true);
+        console.log(query.msg);
+        if (query.query.submit == 'Connect')
+        {
+            irc.CreateSession(ConnectCallback, RecieveCallback);
+            irc.Connect("irc.freenode.net", 6667, null, "vasitronen", null, null);
+            connected = 1;
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><body>', encoding = 'utf-8');
+            response.write("<p>Connecting...</p>", encoding='utf-8');
+            response.end(ind);
+        }
+        else if(query.query.submit == 'Send')
+        {
+            console.log('attempting to send');
+            irc.SendMsg("#newchan", query.query.msg);
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><body>', encoding = 'utf-8');
+            response.end(ind);
+        }
+        else
+        {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><body>', encoding = 'utf-8');
+            response.end(ind);
+        }
 
-	}
-	}).listen(8100);
+    }
+    }).listen(8100);
 setInterval(function () {
 if (connected == 1)
 {
-	connected = 0;
-	irc.Run();
+    connected = 0;
+    irc.Run();
 }
 }, 5000);
