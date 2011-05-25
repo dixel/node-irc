7ea9480994839af71c69bcbcbb8197f89c9598cb
VAvdiushkin <VAvdiushkin@da424b51-ff14-6541-ab8a-bc061a785226>
2011-05-25 06:36:29 +0000

    First commit: README, package.json file, functionality aknowledgement, basic functions desription
    git-svn-id: http://domprog.com:9000/svn/exmashina/IRC/trunk@11 da424b51-ff14-6541-ab8a-bc061a785226

---
+++ src/binding.cc
@@ -0,0 +1,11 @@
+#include <v8.h>
+#include <libircclient/libircclient.h>
+
+using namespace v8;
+
+class IRCClient()
+
+    extern "C" void init(Handle<Object> target)
+    {
+        HandleScope scope;
+    }
