16a72db35ebd0762ac548739ba67abda4f09fe7e
VAvdiushkin <VAvdiushkin@da424b51-ff14-6541-ab8a-bc061a785226>
2011-05-25 06:36:35 +0000

    testing web page 127.0.0.1:8100
    git-svn-id: http://domprog.com:9000/svn/exmashina/IRC/trunk@21 da424b51-ff14-6541-ab8a-bc061a785226

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 var http = require('http');
-var nodeirc = require('./build/default/binding');
 var util = require('util');
-
-console.log('starting nodejs');
+var fs = require('fs');
+var ind;
+var nodeirc = require('./build/default/binding');
 irc = nodeirc.func.prototype;
+var connected = 0;
+fs.readFile('./index.html','utf-8',function (err, data) {
+			if (err) throw err;
+			ind = data; });
+console.log('starting nodejs');
 
 function ConnectCallback()
 {
@@ -22,11 +27,43 @@ function RecieveCallback(origin, message)
 	console.log('%s: %s', origin, message);
 }
 
-irc.CreateSession(ConnectCallback, RecieveCallback);
-irc.Connect("irc.freenode.net", 6667, null, "vasitronen", null, null);
+
 http.createServer(function(request, response){
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.end("hello!\n");
+	if(request.method == 'GET')
+	{
+		query = require('url').parse(request.url, true);
+		console.log(query.msg);
+		if (query.query.submit == 'Connect')
+		{
+			irc.CreateSession(ConnectCallback, RecieveCallback);
+			irc.Connect("irc.freenode.net", 6667, null, "vasitronen", null, null);
+			connected = 1;
+			response.writeHead(200, {"Content-Type": "text/html"});
+			response.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><body>', encoding = 'utf-8');
+			response.write("<p>Connecting...</p>", encoding='utf-8');
+			response.end(ind);
+		}
+		else if(query.query.submit == 'Send')
+		{
+			console.log('attempting to send');
+			irc.SendMsg("#newchan", query.query.msg);
+			response.writeHead(200, {"Content-Type": "text/html"});
+			response.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><body>', encoding = 'utf-8');
+			response.end(ind);
+		}
+		else
+		{
+			response.writeHead(200, {"Content-Type": "text/html"});
+			response.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><body>', encoding = 'utf-8');
+			response.end(ind);
+		}
+
+	}
 	}).listen(8100);
-irc.Run();
-console.log("aFtErThrEad!");
+setInterval(function () {
+if (connected == 1)
+{
+	connected = 0;
+	irc.Run();
+}
+}, 5000);
